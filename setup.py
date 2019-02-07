ee7bd80bea397e516ff2cf44f177fb696dbfd3f1
Patrice Ferlet <metal3d@gmail.com>
2019-02-07 20:34:50 +0100

    Change version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="jpyextra",
-    version="0.0.1",
+    version="0.0.2",
     author="Patrice Ferlet",
     author_email="metal3d@gmail.com",
     description="Some extra tools to work with Jupyter",
